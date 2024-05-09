ebb29e0c65ad14cba333deb097d97eb37c88bc56
Alexander Koskovich <akoskovich@pm.me>
2024-05-09 23:54:22 +0545

    mojito: Update to Boot Control HAL v1.2 Change-Id: Ib5d06c9b0c7e2ed000eb03a9bf18e4d78cc8740d
    Signed-off-by: romgharti <romgharti@gmail.com>

--- device.mk
+++ device.mk
@@ -13,9 +13,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
