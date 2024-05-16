bd9b13f9a514317f5394d7850990cc63beae972e
romgharti <romgharti@gmail.com>
2024-05-16 12:01:16 +0545

    mojito: Bliss ROM Official

--- bliss_mojito.mk
+++ bliss_mojito.mk
@@ -17,6 +17,7 @@ $(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_ESSENTIAL_GAPPS := true
+BLISS_BUILDTYPE := OFFICIAL
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := bliss_mojito
