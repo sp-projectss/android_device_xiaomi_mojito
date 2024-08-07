390951f580784eede23e35c263dc0a8caa00f59c
romgharti <romgharti@gmail.com>
2024-08-07 21:36:34 +0545

    mojito: Setup dalvik vm configs separately - mojito has 4/6 GB RAM variant - Setup dalvik vm according to variant
    Signed-off-by: romgharti <romgharti@gmail.com>

--- init/init_mojito.cpp
+++ init/init_mojito.cpp
@@ -16,7 +16,6 @@
 
 #include <vector>
 
-#include <android-base/logging.h>
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
@@ -32,6 +31,28 @@ void property_override(char const prop[], char const value[], bool add = true) {
         __system_property_update(pi, value, strlen(value));
     else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_dalvikvm_props() {
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram > 5072ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "16m");
+        property_override("dalvik.vm.heapgrowthlimit", "256m");
+        property_override("dalvik.vm.heaptargetutilization", "0.5");
+        property_override("dalvik.vm.heapmaxfree", "32m");
+    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xhdpi-4096-dalvik-heap.mk
+        property_override("dalvik.vm.heapstartsize", "8m");
+        property_override("dalvik.vm.heapgrowthlimit", "192m");
+        property_override("dalvik.vm.heaptargetutilization", "0.6");
+        property_override("dalvik.vm.heapmaxfree", "16m");
+    }
+
+    property_override("dalvik.vm.heapsize", "512m");
+    property_override("dalvik.vm.heapminfree", "8m");
 }
 
 void load_sunny() {
@@ -53,4 +74,5 @@ void vendor_load_properties() {
     if (access("/system/bin/recovery", F_OK) != 0) {
         load_sunny();
     }
+    load_dalvikvm_props();
 }
