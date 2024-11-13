527205e6a984af51929fbaa28ddddb2db5fe744d
K4LCHAKRA <oseverest44@gmail.com>
2024-11-13 13:05:13 +0000

    mojito: Bringup for everest-AOSP

--- everest_mojito.mk
+++ everest_mojito.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
 # Inherit some LineageOS stuffs
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/everest/config/common_full_phone.mk)
 
 # Some Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
@@ -28,7 +28,7 @@ WITH_GAPPS := true
 TARGET_DEFAULT_PIXEL_LAUNCHER := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_mojito
+PRODUCT_NAME := everest_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
